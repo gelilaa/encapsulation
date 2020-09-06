f56f7127c24546fcaff50318a315b16fc9c67496
gelila <gelila.ali@outlook.com>
2020-09-06 11:57:50 +0200

    week-1

--- isolate/03-getters-and-setters/exercises/get-2.js
+++ isolate/03-getters-and-setters/exercises/get-2.js
@@ -9,15 +9,18 @@ const obj1 = {
 const obj2 = {
   numbers: [12, 4, 9, 36, 7, 0, -2],
   modulo: 3,
+  get ZeroRemainders(){
+     return this.numbers.filter((x) => x % this.modulo === 0);
+  }
   // write a getter that matches the behavior of .getZeroRemainders
 };
 
 const obj1ZeroRemainders = obj1._;
-const test1a = deepCompare(obj1ZeroRemainders, [12, 9, 36]);
+const test1a = deepCompare(obj1ZeroRemainders, [12, 9, 36,0]);
 console.assert(test1a, 'Test 1 a');
 
 const obj2ZeroRemainders = obj2._;
-const test1b = deepCompare(obj2ZeroRemainders, [12, 9, 36]);
+const test1b = deepCompare(obj2ZeroRemainders, [12, 9, 36,0]);
 console.assert(test1b, 'Test 1 b');
 
 
@@ -25,11 +28,11 @@ obj1.modulo = 6;
 obj2.modulo = 6;
 
 const obj1ZeroRemainders2 = _;
-const test2a = deepCompare(obj1ZeroRemainders2, [12, 36]);
+const test2a = deepCompare(obj1ZeroRemainders2, [12, 36,0]);
 console.assert(test2a, 'Test 2 b');
 
 const obj2ZeroRemainders2 = _;
-const test2b = deepCompare(obj2ZeroRemainders2, [12, 36]);
+const test2b = deepCompare(obj2ZeroRemainders2, [12, 36,0]);
 console.assert(test2b, 'Test 2 b');
 
 
